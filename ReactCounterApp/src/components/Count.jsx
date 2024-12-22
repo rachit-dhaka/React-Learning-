592a76a02e3f0878f2b61eceb7cf037a1aaf2cfe
Rachit Dhaka <rachitdhaka29@gmail.com>
2024-12-23 00:49:33 +0530

    Use state Hook

--- ReactCounterApp/src/components/Count.jsx
+++ ReactCounterApp/src/components/Count.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Count.css'
 
 const Count = () => {
+  const [count, setCount]=useState(0);
   return (
     <div className="count-container">
-        <p id='title'>The Button is clicked 0 times</p>
-        <button id='btn'>Dabaoo</button>
+        <p id='title'>The Button is clicked {count} times</p>
+        <button id='btn' onClick={()=>{ setCount(count+1)}}>Dabaoo</button>
     </div>
   )
 }
