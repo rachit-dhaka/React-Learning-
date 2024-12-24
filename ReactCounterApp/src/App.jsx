c53f0e4ada8aa844a7b9b8310cb6f39d6fe28c12
Rachit Dhaka <rachitdhaka29@gmail.com>
2024-12-25 01:27:35 +0530

    Using Props , functions and Childrens

--- ReactCounterApp/src/App.jsx
+++ ReactCounterApp/src/App.jsx
@@ -1,17 +1,34 @@
 
 import './App.css'
 import Count from './components/Count'
+import Card from './components/Card'
+import Button from './components/Button'
+import { useState } from 'react'
 
 function App() {
-  
+  const[count,setcount]=useState(0);
+
+  function clicking(){
+    setcount(count+1);
+  }
 
   return (
+
+      
     
-     <div className="something">
-      <Count/>
-     </div>
-    
-  )
-}
+    <div>
+     <Button ifClick={clicking} text="Rachit" >
+
+       <h1> {count} </h1>
+     </Button>
+    </div>
+   
+ )
+  
+
+  }
+
+  
+
 
 export default App
