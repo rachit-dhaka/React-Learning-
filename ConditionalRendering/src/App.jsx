0f352175af5b68fd3a23e1461a1a5652002252c2
Rachit Dhaka <rachitdhaka29@gmail.com>
2024-12-26 21:32:25 +0530

    Conditional Rendering

---
+++ ConditionalRendering/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+import './App.css'
+import LoginBtn from './components/LoginBtn'
+import LogoutBtn from './components/LogoutBtn'
+
+function App() {
+  
+  const[isLoggedIn , setLoggedIn]=useState(true);
+
+
+
+  return ( 
+    <div>
+      {isLoggedIn? <LogoutBtn/> : <LoginBtn/>}
+    </div>
+  )
+
+
+
+  // if(isLoggedIn){
+  //   return (
+  //     <LogoutBtn />
+  //   )
+  // }
+  // else{
+  //   return (
+  //     <LoginBtn />
+  //   )
+  // }
+
+  
+
+}
+
+export default App
