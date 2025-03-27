2f6005917c996dd617d5ea88d286ad648dde6eeb
Rachit Dhaka <rachitdhaka29@gmail.com>
2025-03-27 21:59:01 +0530

    Stop Watch Example of useref

--- UseRef/src/App.jsx
+++ UseRef/src/App.jsx
@@ -4,51 +4,102 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setcount]=useState(1);
 
-  const handleClick=()=>{
-    val.current=val.current+1;
-    console.log("Val ki vlaue hai :" ,val);
+  // V0-Example
+  //     <div>
+  //       const [count, setcount]=useState(1);
+
+  // // const handleClick=()=>{
+  // //   val.current=val.current+1;
+  // //   console.log("Val ki vlaue hai :" ,val);
     
-    setcount(count+1);
-  }
+  // //   setcount(count+1);
+  // // }
 
-  useEffect(() => {
-    console.log("Me render hoo gaya hu ");
+  // // useEffect(() => {
+  // //   console.log("Me render hoo gaya hu ");
     
-  } )
+  // // } )
 
-  let btnRef=useRef();
+  // // let btnRef=useRef();
 
-  const changeColour =()=>{
-    if(btnRef.current.style.backgroundColor==='red'){
-        btnRef.current.style.backgroundColor='blue';
+  // // const changeColour =()=>{
+  // //   if(btnRef.current.style.backgroundColor==='red'){
+  // //       btnRef.current.style.backgroundColor='blue';
+  // //   }
+  // //   else{
+  // //       btnRef.current.style.backgroundColor='red'
+  // //   }
+    
+  // // }
+
+  // // // UseRef Hook
+  // // let val=useRef(1);
+  //     </div>
+
+
+// create a stop watch application\
+    const [time, setTime]=useState(0);
+    let timerRef=useRef(null);
+
+    const TimeStart=()=>{
+      timerRef.current=setInterval(() => {
+        setTime(time=>time+1)
+      }, 1000);
     }
-    else{
-        btnRef.current.style.backgroundColor='red'
-    }
-    
-  }
 
-  // UseRef Hook
-  let val=useRef(1);
+    const TimeStop=()=>{
+      clearInterval(timerRef.current);
+      timerRef.current=null;
+
+    }
+
+    const TimeReset =()=>{
+      TimeStop();
+      setTime(0);
+      
+    }
+
+
+
   
 
 
   return (
-    <div>
-      <button ref={btnRef} onClick={handleClick}>
-        Increment
-      </button>
-      <br />
-      <br />
-      <button onClick={changeColour}>
-        Change the colour of the 1st Button
-      </button>
-      <div>
-        Count : {count}
-      </div>
+      // V0-Example
+    // <div>
+    //   {/* <button ref={btnRef} onClick={handleClick}>
+    //     Increment
+    //   </button>
+    //   <br />
+    //   <br />
+    //   <button onClick={changeColour}>
+    //     Change the colour of the 1st Button
+    //   </button>
+    //   <div>
+    //     Count : {count}
+    //   </div> */}
+    // </div>
+// Creating stop watch application 
+    <div >
+      <p className='nothing'>Stop Watch : {time} Second </p>
+
+       <div className='something'>
+          <button onClick={TimeStart}>
+            Start
+          </button>
+
+          <button onClick={TimeStop}>
+            Stop
+          </button>
+
+          <button onClick={TimeReset}>
+            Reset
+          </button>
+       </div>
     </div>
+
+    
   )
 }
 
